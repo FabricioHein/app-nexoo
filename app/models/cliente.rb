d3e7fd8ed1785da35627819068d2a0673366ed08
fabricio.hein <fabricio.hein@live.com>
2018-06-11 11:52:10 -0400

    final teste

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -4,13 +4,12 @@ class Cliente < ActiveRecord::Base
                                length: {minimum: 2, message: 'deve ter pelo menos 2 caracteres'}
     
     validates :telefone, presence: {message: 'não pode ser deixado em branco Ex (0000-0000)'}, 
-                                   length: {maximum: 9, message: 'deve ter até 11 caracteres'}
+                                   length: {maximum: 9, message: 'deve ter até 9 caracteres'}
     
     validates :email, presence: {message: 'não pode ser deixado em branco'}, 
                               length: {minimum: 10, message: 'deve ter pelo menos 10 caracteres'}, 
                               uniqueness: {message: 'deve ser único'}
     
-    validates :endereco, length: {in: 10..100, message: 'deve ter entre 10 e 100 caracteres'}
+    validates :endereco, length: {in: 3..100, message: 'deve ter entre 3 e 100 caracteres'}
     
-
 end
